55ef46cd9a7644e8a6387c8ac0458a9ad51fbe5b
EC2 Default User <ec2-user@ip-172-31-16-101.ap-northeast-1.compute.internal>
2020-11-28 16:00:45 +0000

    change response

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* POST users listing. */
 router.post('/', function(req, res, next) {
-  res.send(JSON.stringify(req));
+    console.log(req);
+    res.send(JSON.stringify(req));
 });
 
 module.exports = router;
